12d46b9ed0a957ff8cfa4d35bdb689fe8e8cbcac
Mylon Suren <23694620+mylonsuren@users.noreply.github.com>
2022-01-15 18:58:33 -0500

    Update for year 2023

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ export const DEBUG = true;
 
 export const BUDGET = "75";
 
-export const READ_YEAR = "2021";
-export const WRITE_YEAR = "2022";
+export const READ_YEAR = "2022";
+export const WRITE_YEAR = "2023";
 
 export const MSG_TYPES = {
 	DRAWNAME: 0,
